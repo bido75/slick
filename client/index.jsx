40fc85c7580c21d0477c7fcaa04ca45e0dc961ce
rainede <rainey@techgiver.com>
2017-03-06 12:35:44 +0000

    create new react components

--- client/index.jsx
+++ client/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import Channel from './component/Channel.jsx';
+import ChannelDetail from './component/ChannelDetail.jsx';
 
 Meteor.startup(()=>{
   render(
-    <div>Hello, world!</div>,
-    document.getElementById('root'),
+    <div>
+      <Channel/>
+
+      Hello, world!
+    </div>,
+    document.getElementById('root')
   );
 });
